15402f0d06cab021a67c0ff98508faec9ef90aff
Gabriel One <96993016+gabrielluizone@users.noreply.github.com>
2023-08-01 00:12:53 -0300

    Neo Site Update

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Esse arquivo é opcional, mas você pode usá-lo para adicionar lógica de carregamento caso necessário.
-// Por exemplo, para redirecionar automaticamente após um período de tempo.
-// Aqui está um exemplo simples de redirecionamento após 3 segundos:
-
+//
 setTimeout(() => {
-  window.location.href = "https://gabrielluizone.notion.site/Data-Science-Analytics-bff775e4da1d4b0396898644e7bbb75c?pvs=4";
-}, 0); // 0 milissegundos = 3 segundos
+  window.location.href = "https://gabrielluizone.super.site/";
+}, 0); // Tempo em milissegundos
